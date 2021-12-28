215c5a88470d7169851a316b4117214dfe3be9b5
Tony Martinsson <tony.martinsson@hotmail.com>
2021-12-28 11:27:34 +0100

    Added navigationBar and links

--- src/routes/previous-rounds/index.tsx
+++ src/routes/previous-rounds/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import "../../layout/container.css";
+import NavigationBar from "../../components/NavigationBar";
 
 const PreviousRounds = () => (
   <div className="container">
-    <h1>PreviousRounds</h1>
+    <h1>Previous Rounds</h1>
+    <NavigationBar/>
   </div>
 );
 
--- src/routes/scoreboard/index.tsx
+++ src/routes/scoreboard/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import "../../layout/container.css";
+import NavigationBar from "../../components/NavigationBar";
 
 const Scoreboard = () => (
   <div className="container">
     <h1>Scoreboard</h1>
+    <NavigationBar/>
   </div>
 );
 
