21fe09f58419d57238719ebf1f66ea03e67df9e5
Olof-Wallgren <naturel.olof@hotmail.com>
2021-12-29 10:20:08 +0100

    configure hostong and db

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,10 +1,27 @@
-import * as React from "react";
+import { collection, getDocs } from "@firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../../firebase-config";
 import "../../layout/container.css";
 
-const Home = () => (
-  <div className="container">
-    <h1>Home Testing Olof</h1>
-  </div>
-);
+const Home = () => {
+  // const db = firestore();
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "tournaments");
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      data.docs.map((i: any) => {
+        console.log(i.data());
+      });
+    };
+    getUsers();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Home Testing Olof</h1>
+    </div>
+  );
+};
 
 export default Home;
