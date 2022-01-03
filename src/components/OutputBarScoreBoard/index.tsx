7073f1bfb78f7581c58dd41f3f9bdf68281ab957
Timothy Gramnaes <Tgramnaes@gmail.com>
2022-01-03 13:26:29 +0100

    Components up

--- src/components/OutputBarScoreBoard/index.tsx
+++ src/components/OutputBarScoreBoard/index.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "../../layout/OutputBar.css";
-import "./OutputBarRound.css";
+// import "./OutputBarRound.css";
 import { Grid, Button, Menu, MenuItem } from '@mui/material';
 
 
 interface Props {
-  player1?: React.ReactNode,
-  player2?: React.ReactNode,
+  player?: React.ReactNode,
+  totalScore?: React.ReactNode,
+  matchHistory?: React.ReactNode,
   children?: React.ReactNode
 
 }
 
 const OutputBarScoreBoard: React.FC<Props> = ({
-  player1,
-  player2,
+  player,
+  totalScore,
+  matchHistory,
   children
   }) => {
  
@@ -21,17 +23,22 @@ const OutputBarScoreBoard: React.FC<Props> = ({
   return (
     <Grid container className="outputBarContainer" spacing={1}>
 
-      <Grid item xs={9}>
-        <p className="names">{player1} - {player2}</p>
+      <Grid item xs={7}>
+        <p className="names">{player}</p>
+      </Grid>
+
+      <Grid item xs={2}>
+        <p className="totalScore">{totalScore}</p>
       </Grid>
 
       <Grid item xs={3}>
-
-
+        <p className="matchHistory">{matchHistory}</p>
+        <p className="matchHistory">{matchHistory}</p>
+        <p className="matchHistory">{matchHistory}</p>
       </Grid>
 
-     
-    </Grid>
+</Grid>
+
   );
 };
 
--- src/routes/scoreboard/index.tsx
+++ src/routes/scoreboard/index.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import "../../layout/container.css";
 import NavigationBar from "../../components/NavigationBar";
+import OutputbarScoreBoard from "../../components/OutputBarScoreBoard";
 
 const Scoreboard = () => (
   <div className="container">
     <h1>Scoreboard</h1>
     <NavigationBar/>
+    <OutputbarScoreBoard
+      player="Erlef Doedsdufva"
+      totalScore="3"
+      matchHistory="1-0-0"
+      />
+      <OutputbarScoreBoard
+      player="Tony McHallumi"
+      totalScore="0"
+      matchHistory="0-1-0"
+      />
+
   </div>
 );
 
