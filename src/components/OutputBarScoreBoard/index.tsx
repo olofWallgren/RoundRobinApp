186835476abbd106812ba19621a79e90006ebc0d
Timothy Gramnaes <Tgramnaes@gmail.com>
2022-01-03 10:48:27 +0100

    Component created

--- src/components/OutputBarScoreBoard/index.tsx
+++ src/components/OutputBarScoreBoard/index.tsx
@@ -1,28 +1,37 @@
 import React from "react";
-// import { useLocation } from 'react-router-dom';
-import { Link } from "react-router-dom";
 import "../../layout/OutputBar.css";
-import { Grid } from '@mui/material';
+import "./OutputBarRound.css";
+import { Grid, Button, Menu, MenuItem } from '@mui/material';
 
 
 interface Props {
-  players: string,
+  player1?: React.ReactNode,
+  player2?: React.ReactNode,
+  children?: React.ReactNode
 
 }
 
-const OutputBarScoreBoard = () => {
-
+const OutputBarScoreBoard: React.FC<Props> = ({
+  player1,
+  player2,
+  children
+  }) => {
+ 
 
   return (
-    <div className="outputBarContainer">
-      <div>
-        <p className="names">n00by McNoobface - Spelare 2</p>
-      </div>
-      <div>
-        igen
-      </div>
-      <div>och igen</div>
-    </div>
+    <Grid container className="outputBarContainer" spacing={1}>
+
+      <Grid item xs={9}>
+        <p className="names">{player1} - {player2}</p>
+      </Grid>
+
+      <Grid item xs={3}>
+
+
+      </Grid>
+
+     
+    </Grid>
   );
 };
 
