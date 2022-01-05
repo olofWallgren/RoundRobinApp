f0caa38701dca158f6138b2d0f3d8b22128fe853
Tony Martinsson <tony.martinsson@hotmail.com>
2022-01-05 16:46:35 +0100

    Refactoring CSS

--- src/routes/round/index.tsx
+++ src/routes/round/index.tsx
@@ -3,14 +3,12 @@ import "../../layout/container.css";
 import "../../layout/gameContainer.css";
 import "../../layout/primaryBtn.css";
 import "../../layout/secondaryBtn.css";
+import "../round/round.css";
 import NavigationBar from "../../components/NavigationBar";
 import { Divider } from "@mui/material";
-import { CSSProperties } from "react";
 import { Link } from "react-router-dom";
 import OutputBarRound from "../../components/OutputBarRound";
-
 import RoundRobinCalculator from "../../Utilities/RoundRobinCalculator";
-import { Grid } from "@mui/material";
 
 const Round = () => {
   const [disable, setDisable] = React.useState(true); //Använd denna hook för att göra knappen klickbar efter att resultaten är ifyllda
@@ -24,7 +22,14 @@ const Round = () => {
           <div style={{ padding: "1.5rem 0" }}>
             <h3 style={{ margin: "0" }}>Round 1</h3>
           </div>
-          <div style={playerContainer}>
+          <div className="textContainer">
+            <p className="marginBottom">Pairings:</p>
+            <div className="flexColumn">
+              <p className="marginBottom">Result:</p>
+              <p className="wldStyle">W - L - D</p>
+            </div>
+          </div>
+          <div className="playerContainer">
             <OutputBarRound
               player1="Erlef Doedsdufva"
               player2="Tony McHallumi"
@@ -79,58 +84,29 @@ const Round = () => {
             />
           </div>
           <RoundRobinCalculator />
-          <div style={flexBetween}>
+          <div className="flexBetween">
             <div>
-              <p style={paraStyle}>End Tournament</p>
+              <p className="paraStyle">End Tournament</p>
             </div>
             <div>
-              <p style={paraStyle}>Pause Round</p>
+              <p className="paraStyle">Pause Round</p>
             </div>
           </div>
         </div>
-        <div style={btnContainer}>
+        <div className="btnContainer">
           <Link
-            style={btnWidth}
             to="/create-tournament"
-            className="secondaryBtn"
+            className="secondaryBtn btnWidth"
           >
             Back
           </Link>
-          <button style={btnWidth} className="primaryBtn" disabled={disable}>
+          <button className="primaryBtn btnWidth" disabled={disable}>
             Next Round
           </button>
         </div>
       </div>
     </>
   );
-};
-
-const textContainer: CSSProperties = {
-  padding: "0 1rem",
-};
-const paraStyle: CSSProperties = {
-  color: "#FA04F6",
-  cursor: "pointer",
-};
-const flexBetween: CSSProperties = {
-  display: "flex",
-  justifyContent: "space-between",
-  marginTop: "1rem",
-  marginBottom: "0",
-};
-const btnContainer: CSSProperties = {
-  display: "flex",
-  justifyContent: "space-between",
-  marginTop: "2rem",
-  width: "100%",
-};
-const btnWidth: CSSProperties = {
-  width: "48%",
-};
-const playerContainer: CSSProperties = {
-  overflowY: "auto",
-  overflowX: "hidden",
-  height: "350px",
 };
 
 export default Round;
