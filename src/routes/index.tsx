97c1949711fc7320f8d69a516ff327c8d2d7b75f
Tony Martinsson <tony.martinsson@hotmail.com>
2022-01-13 21:32:47 +0100

    Updated routes and typing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,64 +1,70 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Loader from '../components/Loader';
+import Loader from "../components/Loader";
 
-const NotFound = React.lazy(()  => import('./not-found'));
-const Home = React.lazy(()  => import('./home'));
-const Tournament = React.lazy(()  => import('./tournament'));
-const PreviousRounds = React.lazy(()  => import('./previous-rounds'));
-const Round = React.lazy(()  => import('./round'));
-const Scoreboard = React.lazy(()  => import('./scoreboard'));
+const NotFound = React.lazy(() => import("./not-found"));
+const Home = React.lazy(() => import("./home"));
+const Tournament = React.lazy(() => import("./tournament"));
+const PreviousRounds = React.lazy(() => import("./previous-rounds"));
+const Round = React.lazy(() => import("./round"));
+const Scoreboard = React.lazy(() => import("./scoreboard"));
 
 interface IRoute {
-    exact?: boolean,
-    path: string,
-    title: string,
-    component: any,
+  exact?: boolean;
+  path: string;
+  title: string;
+  component: React.LazyExoticComponent<() => JSX.Element>;
 }
 
 export const RouterTree: IRoute[] = [
-    {
-        exact: true,
-        path: '/',
-        title: 'home',
-        component: Home,
-    },
-    {
-        path: '/create-tournament',
-        title: 'Tournament',
-        component: Tournament,
-    },
-    {
-        path: '/current-tournament/previous-rounds',
-        title: 'PreviousRounds',
-        component: PreviousRounds,
-    },
-    {
-        path: '/current-tournament/round',
-        title: 'Round',
-        component: Round,
-    },
-    {
-        path: '/current-tournament/scoreboard',
-        title: 'Scoreboard',
-        component: Scoreboard,
-    },
-    {
-        path: '/not-found',
-        title: 'Not-found',
-        component: NotFound,
-    },
+  {
+    path: "/",
+    title: "Home",
+    component: Home,
+    exact: true,
+  },
+  {
+    path: "/create-tournament",
+    title: "Tournament",
+    component: Tournament,
+  },
+  {
+    path: "/current-tournament/previous-rounds",
+    title: "Previous Rounds",
+    component: PreviousRounds,
+  },
+  {
+    path: "/current-tournament/round",
+    title: "Round",
+    component: Round,
+  },
+  {
+    path: "/current-tournament/scoreboard",
+    title: "Scoreboard",
+    component: Scoreboard,
+  },
+  {
+    path: "/not-found",
+    title: "Not found",
+    component: NotFound,
+  },
 ];
 
 const Router = () => (
-    <React.Suspense fallback={<Loader />} >   
-        <Switch>
-            {RouterTree.map((r: IRoute) =>
-                <Route key={r.path} {...r} />
-            )}
-            <Route component={NotFound} />
-        </Switch>
-</React.Suspense>
+  <React.Suspense fallback={<Loader />}>
+    <Switch>
+      {RouterTree.map((route) => (
+        <Route
+          key={route.title}
+          path={route.path}
+          exact={route.exact}
+          component={route.component}
+        />
+      ))}
+
+      <Route component={NotFound} />
+    </Switch>
+  </React.Suspense>
 );
 
 export default Router;
