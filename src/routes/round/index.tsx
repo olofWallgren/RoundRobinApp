6bf4a770920ca852303841c3525b61d0ec9ff759
Olof-Wallgren <naturel.olof@hotmail.com>
2022-01-20 13:48:17 +0100

    move roundmaker to tournament

--- src/routes/round/index.tsx
+++ src/routes/round/index.tsx
@@ -7,7 +7,7 @@ import "../round/round.css";
 import NavigationBar from "../../components/NavigationBar";
 import { Divider } from "@mui/material";
 import { Link } from "react-router-dom";
-import MakeRoundRobinPairings from "../../Utilities/RoundMaker/roundMaker";
+
 import { TournamentStore } from "../../Contexts/tournamentContext";
 import Timer from "../../components/Timer";
 
@@ -54,7 +54,7 @@ const Round = () => {
         <div className="gameContainer">
           <div className="headingWrapper flexBetween">
             <h3 className="zeroMargin">{`Round-${round + 1}`}</h3>
-            <Timer hours={0} minutes={50} seconds={0}/>
+            <Timer hours={0} minutes={50} seconds={0} />
           </div>
           <div className="textWrapper">
             <p className="alignBottom secondaryColor">Pairings:</p>
@@ -64,11 +64,11 @@ const Round = () => {
             </div>
           </div>
           <div className="playerContainer">
-            <MakeRoundRobinPairings
+            {/* <MakeRoundRobinPairings
               players={playerArray}
               round={round}
               ableNextRound={ableNextRound}
-            />
+            /> */}
           </div>
           <div className="flexBetween">
             <div className="alignBottom">
---
+++ src/types/tournamentArray.ts
@@ -0,0 +1,11 @@
+export type Player = {
+  name: string;
+  id: number;
+};
+
+export type TournamentArray = {
+  player1: Player;
+  player2: Player;
+  matchId: string;
+  matchResult: string;
+}[][];
