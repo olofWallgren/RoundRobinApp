c070c893f161295cb02742f6b7d64ac320dc16de
Tony Martinsson <tony.martinsson@hotmail.com>
2022-01-20 09:42:21 +0100

    Added data list for options

---
+++ src/components/OutputBarRound/optionsDataList.tsx
@@ -0,0 +1,42 @@
+export const optionsDataList = [
+    {
+      label: "Still playing",
+      value: "Still Playing",
+    },
+    {
+      label: "2 - 0 - 0",
+      value: "2 - 0 - 0",
+    },
+    {
+      label: "2 - 1 - 0",
+      value: "2 - 1 - 0",
+    },
+    {
+      label: "1 - 0 - 1",
+      value: "1 - 0 - 1",
+    },
+    {
+      label: "1 - 1 - 1",
+      value: "1 - 1 - 1",
+    },
+    {
+      label: "0 - 0 - 1",
+      value: "0 - 0 - 1",
+    },
+    {
+      label: "1 - 1 - 1",
+      value: "1 - 1 - 1",
+    },
+    {
+      label: "0 - 2 - 0",
+      value: "0 - 2 - 0",
+    },
+    {
+      label: "1 - 2 - 0",
+      value: "1 - 2 - 0",
+    },
+    {
+      label: "0 - 1 - 1",
+      value: "0 - 1 - 1",
+    },
+  ];
