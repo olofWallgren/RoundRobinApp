00f4af0553070ce4173c93c750f179494ed24c54
Tony Martinsson <tony.martinsson@hotmail.com>
2022-01-24 13:09:40 +0100

    Content changed to space between

--- src/components/OutputBarPrevRounds/index.tsx
+++ src/components/OutputBarPrevRounds/index.tsx
@@ -1,34 +1,34 @@
 import React from "react";
 import "../../layout/OutputBar.css";
-import "./OutputBarRound.css";
-import { Grid, Button, Menu, MenuItem } from '@mui/material';
-
+import "../../layout/OutputBar.css";
+import { Grid } from "@mui/material";
 
 interface Props {
-  player?: React.ReactNode,
-  roundResult?: React.ReactNode,
-  children?: React.ReactNode
-
+  player?: React.ReactNode;
+  roundResult?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 const OutputBarPrevRounds: React.FC<Props> = ({
   player,
   roundResult,
-  children
-  }) => {
- 
-
+  children,
+}) => {
   return (
-    <Grid container className="outputBarContainer" spacing={1}>
-
-      <Grid item xs={9}>
+    <Grid 
+      className="outputBarContainer"
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Grid item >
         <p className="names">{player}</p>
       </Grid>
 
-      <Grid item xs={3}>
+      <Grid item >
         <p className="totalScore">{roundResult}</p>
       </Grid>
-     
     </Grid>
   );
 };
